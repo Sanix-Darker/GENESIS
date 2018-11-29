ae2955b8d85959ff8ef53b45be3f24a232264d8e
Ange <s4nixd@gmail.com>
2018-11-29 19:46:33 +0100

    Add permutation method

--- G.py
+++ G.py
@@ -6,13 +6,15 @@
 ####################################
 
 import os
+from itertools import combinations 
+  
 
-print "\n####################################"
-print "# => G E N E S I S <=              #"
-print "# By S4n1x D4rk3r                  #"
-print "# https://github.com/Sanix-Darker  #"
-print "# PYTHON-CAMEROUN                  #"
-print "####################################\n"
+print "\n|-----------------------------------"
+print "| => G E N E S I S <=              |"
+print "| By S4n1x D4rk3r                  |"
+print "| https://github.com/Sanix-Darker  |"
+print "| PYTHON-CAMEROUN                  |"
+print "====================================\n"
 
 # The link of the website
 # keys = raw_input("\nEnter key words seperate by \",\" (Ex: manioc, patate):")
@@ -35,12 +37,43 @@ array_keys = keys.split(',')
 file_password = open("passwords.txt", "w+");
 
 # loop throught possibilities 
-for password in array_keys:
-    i = 0
-    while i < Superfux.count:
-        # Write in the password file
-        file_password.write(password+Superfux[i]+"\n")
-        i = i+1
+# Python function to print permutations of a given list 
+def permutation(lst): 
+  
+    # If lst is empty then there are no permutations 
+    if len(lst) == 0: 
+        return [] 
+  
+    # If there is only one element in lst then, only 
+    # one permuatation is possible 
+    if len(lst) == 1: 
+        return [lst] 
+  
+    # Find the permutations for lst if there are 
+    # more than 1 characters 
+  
+    l = [] # empty list that will store current permutation 
+  
+    # Iterate the input(lst) and calculate the permutation 
+    for i in range(len(lst)): 
+       m = lst[i] 
+  
+       # Extract lst[i] or m from the list.  remLst is 
+       # remaining list 
+       remLst = lst[:i] + lst[i+1:] 
+  
+       # Generating all permutations where m is first 
+       # element 
+       for p in permutation(remLst): 
+           l.append([m] + p) 
+    return l 
+  
+  
+# Driver program to test above function 
+data = list('123') 
+for p in permutation(data):
+    file_password.write(p+"\n")
+    print p
 
 # Pause the console
 os.system("pause")
