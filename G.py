db462bbf399819aab0cfc4035287e6e85fc0742f
Ange <s4nixd@gmail.com>
2018-11-29 16:05:44 +0100

    Getting parameters from the user(keys word)

---
+++ G.py
@@ -0,0 +1,25 @@
+####################################
+# => G E N E S I S <=              #
+# By S4n1x D4rk3r                  #
+# https://github.com/Sanix-Darker  #
+# PYTHON-CAMEROUN                  #
+####################################
+
+import os
+
+print "\n####################################"
+print "# => G E N E S I S <=              #"
+print "# By S4n1x D4rk3r                  #"
+print "# https://github.com/Sanix-Darker  #"
+print "# PYTHON-CAMEROUN                  #"
+print "####################################\n"
+
+# The link of the website
+keys = raw_input("\nEnter key words seperate by \",\" (Ex: manioc, patate):")
+# Superfux
+Superfux = ['0','1','2','3','4','5','6','7','8','9','!','@','#','$','%','^','&','*','/','<','>']
+# array Keys
+array_keys = keys.split(',')
+
+# Pause the console
+os.system("pause")
