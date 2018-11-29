092171aa3b46826febeb02a501b107b2397531ed
Ange <s4nixd@gmail.com>
2018-11-29 16:16:56 +0100

    Adding Superflux caracters

--- G.py
+++ G.py
@@ -17,9 +17,19 @@ print "####################################\n"
 # The link of the website
 keys = raw_input("\nEnter key words seperate by \",\" (Ex: manioc, patate):")
 # Superfux
-Superfux = ['0','1','2','3','4','5','6','7','8','9','!','@','#','$','%','^','&','*','/','<','>']
+Superfux = ['A','B','C','D','E','F','G','H','I','J',
+            'K','L','M','N','O','P','Q','R','S','T',
+            'U','V','W','X','Y','Z','a','b','c','d',
+            'e','f','g','h','i','j','k','l','m','n',
+            'o','p','q','r','s','t','u','v','w','x',
+            'y','z',';',',','.','0','1','2','3','4',
+            '5','6','7','8','9','!','@','#','$','%',
+            '^','&','*','/','<','>','|',']','[','}',
+            '{']
 # array Keys
 array_keys = keys.split(',')
+
+
 
 # Pause the console
 os.system("pause")
